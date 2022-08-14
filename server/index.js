b89e1920223bf9612fd997c257377acf547d35d2
anurag70 <anuragmisraphy99@gmail.com>
2022-08-14 23:20:46 +0530

    envAdd

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
+dotenv.config();
 import postsRouter from './routes/posts.js';
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postsRouter);
 
-const CONNECTION_URL = 'mongodb+srv://anurag007:Anurag123@cluster0.qj5teic.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
