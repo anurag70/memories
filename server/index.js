5634176f6adc12fb182f1b10e6731805ca9ed45b
anurag70 <anuragmisraphy99@gmail.com>
2022-08-14 22:57:34 +0530

    mongoDB

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/posts', postsRouter);
 
-const CONNECTION_URL = 'mongodb+srv://anurag70:Anurag123@cluster0.0um2k.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://anurag007:Anurag123@cluster0.qj5teic.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
